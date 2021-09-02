4b20a248cb3f35a83efd1fe016a5d1a80fcb8e17
Anna Szwal <aniaszwal@gmail.com>
2021-09-02 13:59:06 +0200

    Error:  Failed to decode param '/%PUBLIC_URL%/favicon.ico

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 
-app.use(express.static(path.join(__dirname, '/client/public')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: false })); //ten middleware umożliwia obsługę formularzy x-www-form-urlencoded
 app.use(express.json()); //middleware, który odbiera dane w formacie JSON (mogą być wysyłane za pomocą form-data)
 app.use(cors()); // middleware cors pozwala w łatwy sposób odblokować wszystkie połączenia. Dzieki cors Możemy np. ustawić, że nasze API pozwala na połączenie tylko i wyłączne z konkretnej domeny oraz z konkretnych metod, albo – co istotne dla publicznych API – z każdej domeny.
@@ -19,7 +20,7 @@ app.use('/api', concertsRoutes); // add concerts routes to server
 app.use('/api', seatsRoutes); // add seats routes to server
 app.use('/api', testimonialsRoutes); // add testimonials routes to server
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/public/index.html'));
 });
 
@@ -27,6 +28,8 @@ app.use((req, res) => {
     res.status(404).send('404 not found...');
 })
 
+// zmienić port serwera. Zamiast sztywnej wartości powinniśmy pozwalać na pobieranie jej ze zmiennych konfiguracyjnych Heroku.
+//Teraz lokalnie wciąż będziemy korzystali z portu 8000, ale Heroku na repo zdalnym będzie wykorzystywać informacje zapisane w konfiguracji.
 app.listen(process.env.PORT ||  8000, () => {
     console.log('Server is running on port: 8000');
 });
